00532cab0cc3c417f7ec3daee9345365b4ba6ce7
Borys Ziukov <ziukoff1985@gmail.com>
2025-01-02 12:16:37 +0200

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-// Імпортуємо необхідні функції та компоненти
+// Імпортуємо React-хуки та стилі
 import { useState, useEffect } from 'react';
 import './App.css';
-// Початковий список контактів з файлу JSON
-import ContactListData from './assets/ContactListData.json';
+
+import ContactListData from './assets/ContactListData.json'; // Початковий список контактів з файлу JSON
 import ContactList from './components/ContactList/ContactList'; // Компонент для відображення списку контактів
 import SearchBox from './components/SearchBox/SearchBox'; // Компонент для пошуку контактів
 import ContactForm from './components/ContactForm/ContactForm'; // Компонент для додавання нового контакту
 
 // Основна функція додатку
 function App() {
-  // Ініціалізуємо стан для контактів
+  // Ініціалізуємо стан для списку контактів
   const [contacts, setContacts] = useState(() => {
     // Отримуємо збережені контакти з localStorage
     const savedContactsData = localStorage.getItem('contactList');
-    // Якщо є збережені контакти, повертаємо їх (розпарсивши JSON), інакше використовуємо початкові дані
+    // Якщо є збережені контакти, повертаємо їх (розпарсивши JSON), інакше використовуємо початкові дані (з файлу ContactListData.json)
     return savedContactsData ? JSON.parse(savedContactsData) : ContactListData;
   });
 
-  // Використовуємо useEffect для збереження списку контактів у localStorage при кожній зміні списку
+  // Використовуємо useEffect для збереження списку контактів у localStorage при кожній зміні списку (стану `contacts`)
   useEffect(() => {
     // Зберігаємо поточний стан контактів у localStorage у вигляді JSON-строки
     localStorage.setItem('contactList', JSON.stringify(contacts));
   }, [contacts]); // Ефект спрацьовує при зміні `contacts`
 
-  // Ініціалізуємо стан для пошукового запиту
-  const [filter, setFilter] = useState(''); // Початковий фільтр порожній
+  // Ініціалізуємо стан для пошуку контактів за ім'ям
+  const [searchByName, setSearchByName] = useState(''); // Початковий фільтр порожній
 
   // Функція для додавання нового контакту
   const addNewContact = newContact => {
@@ -34,7 +34,7 @@ function App() {
     });
   };
 
-  // Функція для видалення контакту
+  // Функція для видалення контакту за його ID
   const deleteContact = contactId => {
     // Оновлюємо стан контактів, відфільтровуючи (залишаючи) ті, що не відповідають переданому ID (видаленого елемента)
     setContacts(prevContacts => {
@@ -43,22 +43,22 @@ function App() {
     });
   };
 
-  // Функція для фільтрування списку контактів
-  // Фільтруємо контакти, які відповідають пошуковому запиту
+  // Функція для фільтрування списку контактів (за ім'ям)
+  // Фільтруємо контакти методом filter(), які відповідають пошуковому запиту
   const visibleContacts = contacts.filter(contact => {
     // Порівнюємо ім'я контакту (у нижньому регістрі) з пошуковим запитом
-    return contact.name.toLowerCase().includes(filter.toLowerCase());
+    return contact.name.toLowerCase().includes(searchByName.toLowerCase());
   });
 
   // Рендеринг компонента
   return (
     <>
       <h1 className="title">Phonebook</h1> {/* Заголовок */}
-      <ContactForm onAdd={addNewContact} />{' '}
+      <ContactForm onAdd={addNewContact} />
       {/* Форма для додавання нового контакту */}
-      <SearchBox value={filter} onFilter={setFilter} />{' '}
+      <SearchBox value={searchByName} onFilter={setSearchByName} />
       {/* Поле для введення пошукового запиту */}
-      <ContactList contacts={visibleContacts} onDelete={deleteContact} />{' '}
+      <ContactList contacts={visibleContacts} onDelete={deleteContact} />
       {/* Список видимих контактів */}
     </>
   );
